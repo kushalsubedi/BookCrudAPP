4312d0f7d183d2c57a90c22fa096c575d5d96e84
kushal <kushal@DESKTOP-F5HJI0Q>
2023-07-23 00:33:23 +0545

    Add project files.

---
+++ BookCrudAPP/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using BookCrudAPP.Data;
+using BookCrudAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCrudAPP.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _data;
+      // Read Data from Database 
+        public CategoryController(ApplicationDbContext data)
+        {
+            _data = data;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objectCategoryList=_data.Categories;
+            return View(objectCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+
+            _data.Categories.Add(obj);
+            _data.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ BookCrudAPP/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookCrudAPP.Models;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace BookCrudAPP.Data;
+
+    public class ApplicationDbContext : DbContext
+    {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+            
+    }
+    public DbSet<Category> Categories { get; set; }
+                
+        
+
+    }
+
